c06e8dfc98c33fa964043b16bb6a80f45bd0a693
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2022-11-12 08:11:43 -0800

    Create 2465. Number of Distinct Averages.cpp

---
+++ BIWEEKLY_91/2465. Number of Distinct Averages.cpp
@@ -0,0 +1,28 @@
+PROBLEM:https://leetcode.com/contest/biweekly-contest-91/problems/number-of-distinct-averages/
+VIDEO:
+
+/*
+METHOD 1
+TC: O(N) + NLOG(N)
+SC: O(N/2)=O(N)
+*/
+
+class Solution {
+public:
+    int distinctAverages(vector<int>& nums) {
+      
+      set<float>s;
+      sort(nums.begin(),nums.end()); //nlogn
+      int n=nums.size();
+      int i=0,j=n-1;
+      while(i<j){
+        s.insert((nums[i]+nums[j])/2.0);
+        nums.erase(nums.begin()+i);
+        //nums.pop_front();
+        nums.pop_back();
+        i++;
+        j--;
+      }
+      return s.size();
+    }
+};
